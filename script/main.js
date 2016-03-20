bca46fc61831a04f44de1fe1580c4d2587c327d5
Lukas Steinbrecher <steinbrecherluk@gmail.com>
2016-03-20 15:56:25 +0100

    changes

--- script/main.js
+++ script/main.js
@@ -1,24 +1,28 @@
 
-    $(document).ready(function() 
-    {
+$(document).ready(function() 
+{
+	$('#mail-link').click(function(e) {
+		var mail = "lukas";
+		window.location.href = "mailto:" + mail + "@lukstei.com";
+	});
 
-		var options=
-		{
-			tab						: 'left',
-			className				: 'checkmark',			
-			slideImageIndexPage		: 1,
-			slideImageIndexPageHome	: 1,
-			showMenuSlider			: true,
-			showMenuAtStart			: true,
-			startPage				: 'menu'
-		};
-		
-        var slide=
-		[
-			{image:'media/image/background/01.jpg',title:''},
-			{image:'media/image/background/02.jpg',title:''},
-		];
-		
-		
-        $('#nostalgia').nostalgia(options,{},slide,[],{},{});
-    });
+	var options=
+	{
+		tab						: 'left',
+		className				: 'checkmark',			
+		slideImageIndexPage		: 1,
+		slideImageIndexPageHome	: 1,
+		showMenuSlider			: true,
+		showMenuAtStart			: true,
+		startPage				: 'menu'
+	};
+	
+    var slide=
+	[
+		{image:'media/image/background/02.jpg',title:''},
+		{image:'media/image/background/01.jpg',title:''},
+	];
+	
+	
+    $('#nostalgia').nostalgia(options,{},slide,[],{},{});
+});
